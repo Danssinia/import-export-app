b317c02b4f0828095277532cc4a3097d96a34692
danssinia <danmerethio19@gmail.com>
2026-03-11 16:38:05 +0300

    swagger API Documentation is configured

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets('uploads',{
     prefix: 'uploads/'
   })
+
+  const config = new DocumentBuilder()
+  .setTitle('Import Export API')
+  .setDescription('complete API for the APP')
+  .setVersion('1.0')
+  .addTag('import')
+  .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api',app,documentFactory)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
